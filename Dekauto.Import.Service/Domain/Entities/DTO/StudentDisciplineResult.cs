6e3bcc5c65279082511ad40361bb4ee976d32134
TOXYGENCY <delorean2026@ya.ru>
2026-04-12 10:29:00 +0300

    feat(dto): добавить поля порядка и флаг внимания в DTO оценки

--- Dekauto.Import.Service/Domain/Entities/DTO/StudentDisciplineResult.cs
+++ Dekauto.Import.Service/Domain/Entities/DTO/StudentDisciplineResult.cs
@@ -10,5 +10,9 @@ namespace Dekauto.Import.Service.Domain.Entities
 
         public double? AudHours { get; set; }
         public double? CreditUnits { get; set; }
+
+        public int? PlanOrder { get; set; }
+
+        public bool RequiresManualValidation { get; set; }
     }
 }
